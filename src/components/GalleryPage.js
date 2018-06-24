c95c11d00591680f627ba05e30ae3e3b289c9487
egtcwaa <egtcwaa.nz@gmail.com>
2018-06-24 22:44:32 +1200

    Update GalleryPage.js

--- src/components/GalleryPage.js
+++ src/components/GalleryPage.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import async from '../async'
-import { Divider } from 'antd';
 
-let GalleryCollection = async(() => import("./GalleryCollection"));
 let EvolvedGallery = async(() => import("./EvolvedGallery"));
 
 
 class GalleryPage extends React.Component {
     render() {
         return (
-            // <div>
-            //     <h1>Gallery</h1>
-            //     <Divider />
-            //     <div>
-            //         <EvolvedGallery />
-            //     </div>
-            // </div>
             <EvolvedGallery />
-
         )
     }
 }
